f3f48d673a52886e9f728f14687fef3a380041a4
Zidni Dziaulhaq <zidnidziaulhaq@mail.ugm.ac.id>
2024-09-17 11:25:40 +0700

    Create Diskriminan.cpp

---
+++ Week 2/Diskriminan/Diskriminan.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    float D, a, b, c, x1, x2, x;
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+    cout << "Enter the value of c: ";
+    cin >> c;
+    
+    D = b*b - 4 * a * c;
+    if (a == 0){
+        cout << "value of a can't be zero!";
+    }
+    else if (D > 0) {
+        x1 = (-b - sqrt(D)) / (2 * a);
+        x2 = (-b + sqrt(D)) / (2 * a);
+        cout << "x1 = " << x1 << " and x2 = " << x2 << endl;
+    } 
+    else if (D == 0) {
+        x = -b / (2 * a);
+        cout << "x = " << x << endl;
+    } 
+    else {
+        cout << "The roots are imaginary (complex)." << endl;
+    }
+
+    return 0;
+}
