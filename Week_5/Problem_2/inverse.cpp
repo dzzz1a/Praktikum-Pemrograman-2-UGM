4397bfee09f3a93d4fd8db0f2d634e2c21a63c44
Zidni Dziaulhaq <zidnidziaulhaq@mail.ugm.ac.id>
2024-09-26 20:31:41 +0700

    Update inverse.cpp

--- Week_5/Problem_2/inverse.cpp
+++ Week_5/Problem_2/inverse.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+vector<int> inverse(const vector<int>& List) {
+    int n = List.size();
+    vector<int> CList(n);
+
+    for (int i = 0; i < n; ++i) {
+        CList[i] = List[n - 1 - i];
+    }
+
+    return CList;
+}
+
+int main() {
+    char o;
+    cout << "User input? (Y/N): ";
+    cin >> o;
+
+    if(o == 'N'){
+        vector<int> List = {1, 2, 3};
+        vector<int> invertedList = inverse(List);
+
+        for (int num : invertedList) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    else{
+        int n;
+    
+        cout << "Enter the value of n: ";
+        cin >> n;
+        vector<int> List(n);
+        for(int i = 0; i < n; i++){
+            cout << "Enter the " << i << " index: ";
+            cin >> List[i];
+        }
+        vector<int> CList = inverse(List);
+
+        for(int num : CList){
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
