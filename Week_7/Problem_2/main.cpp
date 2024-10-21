ce3e29734dd1d6cd7f23381f4d012704c3223b1f
Zidni Dziaulhaq <zidnidziaulhaq@mail.ugm.ac.id>
2024-10-21 20:10:38 +0700

    Create main.cpp

---
+++ Week_7/Problem_2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Factorial of " << n << " = " << factorial(n);
+    return 0;
+}
