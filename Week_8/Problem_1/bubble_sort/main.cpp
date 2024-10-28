026d1428783b70a4957a96e12fbf7f5d4b7a9d58
Zidni Dziaulhaq <zidnidziaulhaq@mail.ugm.ac.id>
2024-10-28 16:31:34 +0700

    Create main.cpp

---
+++ Week_8/Problem_1/bubble_sort/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    long int id;
+    string name;
+    int score;
+};
+
+void BubbleSort(Student arr[], int n, int c) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if ((c == 1 && arr[j].id < arr[j + 1].id) || 
+                (c == 3 && arr[j].score < arr[j + 1].score)) {
+                Student temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void PrintArray(Student arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i].id << " " << arr[i].name << " " << arr[i].score << endl;
+    }
+}
+
+int main() {
+    Student arr[7] = {
+        {9960312699, "Handi Ramadhan", 90},
+        {9963959682, "Rio Alfandra", 55},
+        {9950310962, "Ronaldo Valentino Uneputty", 80},
+        {9970272750, "Achmad Yaumil Fadjri R.", 60},
+        {9970293945, "Alivia Rahma Pramesti", 70},
+        {9952382180, "Ari Lutfianto", 65},
+        {9965653989, "Unknown", 60}
+    };
+    int n = 7;
+
+    BubbleSort(arr, n, 1);
+    cout << "\nArray sorted by ID:\n";
+    PrintArray(arr, n);
+
+    BubbleSort(arr, n, 3);
+    cout << "\nArray sorted by Score:\n";
+    PrintArray(arr, n);
+
+    return 0;
+}
