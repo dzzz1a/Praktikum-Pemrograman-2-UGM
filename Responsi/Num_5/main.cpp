909bae84668c15d60622bdf96e26b04f1f6d74df
Zidni Dziaulhaq <zidnidziaulhaq@mail.ugm.ac.id>
2024-11-25 16:12:44 +0700

    Update main.cpp

--- Responsi/Num_5/main.cpp
+++ Responsi/Num_5/main.cpp
@@ -7,26 +7,26 @@ int main() {
     char m;
     cout << "Do you want a hardcoded input? (y/n): ";
     cin >> m;
-    if (m == 'n') {
+    if (m == 'n') { // if the user chose to input, the initial value of s will be overwrited
         cout << "Enter a string: ";
         cin >> s;
     }
-    int n = s.size();
-    string r(n, '\0');
+    int n = s.size(); // using the function to find the size of an array
+    string r(n, ' '); // assigning the size of the string
     for (int i = 0; i < n; i++) {
         r[i] = s[n - i - 1];
     }
 
     for(int i = 0; i < n; i++) {
         if (s[i] != r[i]) {
-            cout << "the initial string is "<< s <<" and it is not a palindrome" << endl;
+            cout << "the initial string is "<< s <<" and it is NOT a palindrome" << endl;
             m = 'f';
             return 0;
         }
     }
 
     if (m != 'f') {
-        cout << "the initial string is "<< s <<" and it is a palindrome" << endl;
+        cout << "the initial string is "<< s <<" and it IS a palindrome" << endl;
     }
 
     return 0;
