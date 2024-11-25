d388620065e805ddab2e21e19a6b968b49c41eee
Zidni Dziaulhaq <zidnidziaulhaq@mail.ugm.ac.id>
2024-11-25 15:35:04 +0700

    Create main.cpp

---
+++ Responsi/Num_3/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int s[n] = {1, 2, 3, 4, 5}, e = 0, o = 0;
+    char m;
+    cout << "Do you want a hardcoded input and output? (y/n): ";
+    cin >> m;
+    if (m == 'n') {
+        int n;
+        cout << "Enter the size of array: ";
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cout << "Enter the " << i + 1 << "th element: ";
+            cin >> s[i];
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        if (s[i] % 2 == 0) {
+            e++;
+        } else {
+            o++;
+        }
+    }
+
+    cout << "The sum of odd numbers is: " << o << endl;
+    cout << "The sum of even numbers is: " << e << endl;
+    return 0;
+}
